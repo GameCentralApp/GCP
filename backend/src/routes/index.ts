175904898ce6d826ce3cfdf16c718891d762884d
GameCentral <47630301+GameCentralApp@users.noreply.github.com>
2025-08-28 09:29:26 +0100

    Remove problematic files causing build errors

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,6 @@
+// Export all route modules
+export { default as authRoutes } from './auth';
+export { default as usersRoutes } from './users';
+export { default as serversRoutes } from './servers';
+export { default as templatesRoutes } from './templates';
+export { default as settingsRoutes } from './settings';
